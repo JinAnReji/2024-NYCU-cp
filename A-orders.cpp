163e5a2e414ed8e4c0a99125904a467e9027398d
JinAn <146332651+JinAnReji@users.noreply.github.com>
2024-05-21 02:00:33 +0800

    Create A-orders.cpp

---
+++ A-orders.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin>>n;
+    vector<int> c(n); // item cost
+    for(auto& ele: c) cin>>ele;
+    int m; cin>>m;
+    vector<int> s(m); // total cost of an order
+    for(auto& ele: s) cin>>ele;
+    
+    // determin 3 cases:
+    // 1. unique order?
+    // 2. impossible order?
+    // 3. more than one ways to give such sum?
+
+  // Still working on ...
+    
+    
+    return 0;
+}
