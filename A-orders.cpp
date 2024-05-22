e9553213679e141014732e80b19ca4564f448a8d
JinAn <146332651+JinAnReji@users.noreply.github.com>
2024-05-22 17:47:48 +0800

    AC

--- A-orders.cpp
+++ A-orders.cpp
@@ -1,13 +1,17 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cassert>
 using namespace std;
+
+vector<int> dp;
 
 int main(){
     int n; cin>>n;
-    vector<pair<int,int> > c(n); // item cost
-    for(int i=0; i<n; i++) {
-        c[i].first=i;
-        cin>>c[i].second;
-    }
+    vector<int> menu(n); // item price
+    for(auto& ele: menu) cin>>ele;
+
     int m; cin>>m;
     vector<int> s(m); // total cost of an order
     int max_order=0;
@@ -16,59 +20,50 @@ int main(){
         max_order = max(max_order, ele);
     }
     
-    // determin 3 cases:
-    // 1. unique order? print the index out
-    // 2. impossible order?
-    // 3. more than one ways to give such sum?
-    
-    struct
-    {
-        bool operator()(pair<int,int> a, pair<int,int> b) const { return a.second < b.second; }
-    } customLess;
-    
-    // reorder the prices of each item into acscending order
-    sort(c.begin(), c.end(), customLess);
-    vector<int> dp(max_order+1,-1);  // record the index of item if the sum can be achieved  
-    // use the cheapest item to initialize
-    for( int j=1; j<=max_order ; j++){
-        if(j>=c[0].second && j%c[0].second==0)
-            dp[j] = c[0].first; // record the item index
-        else{
-            dp[j]=0;
-        } 
-    }
-     // compute the answer for each order
-    for(int o=0; o<m; o++){
-         // go through items from low cost to higher cost
-        for(int i=1; i<n; i++){
-            // go from small order to the larger order
-            for(int j=1; j<=s[o]; j++){
-                int diff = j-c[i].second;
-                if(diff<0 || dp[j]==-2) continue; // skip if the sum is not enough to order the curr item i
-                if (diff%c[i].second==0){ // find an explicit sol for dp[j]
-                    if(dp[j]) dp[j]=-2; // dp[j] already have a sol, mark -2: Ambiguous
-                    else dp[j]=c[i].first;// dp[j] have no sol yet, store the item index
+    dp.assign(max_order+1,0);
+    // go through every item
+    for(int q=0; q<n; q++){
+        int x = menu[q];
+        if(dp[x]==0){
+            dp[x]=q+1;
+            // go through every possible sum, to fill up the dp table correctly
+            for(int j=0; j<max_order+1-x; j++){
+                if(dp[j]==0) continue; // skip the prices that can't be given
+                if(dp[j]==-1){ 
+                    dp[j+x] = -1; // if dp[j] has multi sol, then dp[j+x] also has multi sol
+                    continue;
                 }
-                else{ // check the table for sol (may fail to find one)
-                    if(dp[diff] && dp[j]) dp[j]=-2; // if dp[diff] has a sol and dp[j] also already has a sol: Ambiguous
-                    else if (dp[diff]>=1) dp[j]=c[i].first; // if dp[diff] has a non-ambiguous sol and dp[j] has no sol yet: record the curr item index
+                // following are case that dp[j] has unique sol
+                if(dp[j+x]!=0){ // if dp[j+x] has at least a sol, then we have found another sol for dp[j+x]
+                    dp[j+x]=-1;
+                    continue;
                 }
+                // following are case that dp[j+x] has no sol yet
+                dp[j+x]=q+1;
             }
-        }  
-        
-        // output the answer
-        if(dp[s[o]]==-2) cout<<"Ambiguous\n";
-        else if(dp[s[o]]==0) cout<<"Impossible\n";
-        else{
-            list<int> ans;
-            for(int i=s[o]; i>0; ){
-                assert(dp[i]>=1);
-                ans.push_front(dp[i]);
-                i -= dp[i];
-            }
-            for(auto& ele: ans) cout<<ele<<" ";
-            cout<<"\n";
         }
+        else{
+            dp[x]=-1;
+            for(int j=1; j<max_order+1-x; j++){
+                if(dp[j]!=0) dp[j+x] = -1;
+            }
+        }
+    }
+    // now just print out the answer
+    for(int i=0; i<m; i++){
+        int order=s[i];
+        if(dp[order]==0) cout<<"Impossible";
+        else if(dp[order]==-1) cout<<"Ambiguous";
+        else{
+            vector<int> ans;
+            for(int p=order; p>0; ){
+                ans.push_back(dp[p]);
+                p -= menu[dp[p]-1];
+            }
+            sort(ans.begin(), ans.end());
+            for(auto& ele: ans) cout<<ele<<" ";
+        }
+        cout<<"\n";
     }
     return 0;
 }
